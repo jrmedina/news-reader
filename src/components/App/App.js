5dff4ba769010f48adb78146d48b87e7696a4d92
jmedina <ndgns2@gmail.com>
2022-10-31 14:54:38 -0600

    Feat: Add detail view of arrticle

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,24 +2,28 @@ import { fetchArticles } from "../../utils/apiCalls";
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import ArticleContainer from "../ArticleContainer/ArticleContainer";
+import DetailCard from "../../DetailCard/DetailCard";
 
 const App = () => {
-
-  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetchArticles("home").then((res) => setArticles(res.results));
+    setLoading(true);
+    fetchArticles("home").then((res) => {
+      setArticles(res.results);
+      setLoading(false);
+    });
   }, []);
-
-
-
-
-
-  return <main>
-    <h1>Times Reader</h1>
-    <ArticleContainer articles={articles}/>
-    
-  </main>;
+  return loading ? (
+    <p>loading...</p>
+  ) : (
+    <main>
+      <h1>Times Reader</h1>
+      {/* <ArticleContainer articles={articles}/> */}
+      <DetailCard article={articles[3]} />
+    </main>
+  );
 };
 
 export default App;
